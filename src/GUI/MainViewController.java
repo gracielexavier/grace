7fda7e45308993d60a9e5642283281237348f3f3
gracielexavier <graciele.xavier.nascimento@hotmail.com.br>
2020-07-04 22:52:14 -0300

    view About

--- src/GUI/MainViewController.java
+++ src/GUI/MainViewController.java
@@ -1,10 +1,16 @@
 package GUI;
 
+import static GUI.util.Alertas.showAlert;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -45,5 +51,15 @@ public class MainViewController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
        
+    }
+    
+    private void loadView(String absoluteName) {
+      try{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+        VBox newVBox = loader.load();
+      }
+      catch (IOException e){
+          showAlert("IO Exception", "Error loading view", e.getMessage(),AlertType.ERROR);
+      }
     }
 }
---
+++ src/GUI/util/Constraints.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI.util;
+
+import static java.lang.Double.max;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author 55819
+ */
+public class Constraints {
+    public static void setTextFieldInteger(TextField txt){
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*")){
+                txt.setText(oldValue);
+            }
+        });
+    }
+    
+    public static void settextFieldMaxLehgth(TextField txt, int max){
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && newValue.length() > max){
+                txt.setText(oldValue);
+            }
+        });
+    }
+    
+    public static void setTextFieldDouble(TextField txt){
+        txt.textProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")){
+                txt.setText(oldValue);
+            }
+        });
+       
+    }
+}
